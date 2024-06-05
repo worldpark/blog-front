a3dcf855363ec5ebccc81fb54185835abe33d22e
김재력 <rlawofur77@gmail.com>
2024-06-05 16:57:42 +0900

    블로그 - front 최초 생성

--- src/App.js
+++ src/App.js
@@ -1,25 +1,34 @@
-import logo from './logo.svg';
 import './App.css';
+import {useState} from "react";
+import {Box, Container, Grid, Link} from "@mui/material";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+    let [title, setTitle] = useState('개발자 블로그');
+
+
+    return (
+        <div>
+            <div className="black-nav">
+                <h4>{title}</h4>
+            </div>
+            <Container>
+                <Grid container spacing={2}>
+                    <Grid item lg={4} xs={12}>
+                        <Box display="flex" flexDirection={"row"}>
+
+                            <Link href="#">menu1</Link>
+                            <Link href="#">menu2</Link>
+                            <Link href="#">menu3</Link>
+                        </Box>
+                    </Grid>
+                    <Grid item lg={8} xs={12}>
+                        content
+                    </Grid>
+                </Grid>
+            </Container>
+        </div>
+    );
 }
 
 export default App;
